e377f2124e07606d7a3d902bdb0a189715031102
Florian Birk <birk.florian95@gmail.com>
2020-04-13 20:24:44 +0200

    crreated basic sidebar...polishing needed

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,121 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="d-flex" id="wrapper" :class="{toggled: this.isToggled}">
+      <div class="bg-light border-right" id="sidebar-wrapper">
+        <div class="sidebar-heading">HSKE Media-Streaming</div>
+        <div class="list-group list-group-flush">
+          <router-link class="list-group-item active" to="/">Home</router-link>
+          <router-link class="list-group-item" to="/about">About</router-link>
+        </div>
+      </div>
+
+      <div id="page-content-wrapper">
+        <div class="container-fluid">
+          <button
+            class="btn btn-primary float-right"
+            id="menu-toggle"
+            @click="toggleMenu"
+          >Toggle Menu</button>
+          <router-view />
+        </div>
+      </div>
     </div>
-    <router-view />
   </div>
 </template>
 
-<style>
+<script>
+export default {
+  name: "app",
+  components: {},
+  data() {
+    return {
+      isToggled: false
+    };
+  },
+  computed: {},
+  methods: {
+    toggleMenu() {
+      this.isToggled = !this.isToggled;
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+@import "./style.scss";
+
+body {
+  overflow-x: hidden;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  @extend .font;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  background-color: $gray;
+  color: $white;
 }
 
-#nav {
-  padding: 30px;
+#sidebar-wrapper {
+  min-height: 100vh;
+  margin-left: -15rem;
+  -webkit-transition: margin 0.25s ease-out;
+  -moz-transition: margin 0.25s ease-out;
+  -o-transition: margin 0.25s ease-out;
+  transition: margin 0.25s ease-out;
+  background-color: $black !important;
+  color: $white;
+  border-right: none !important;
+
+  @extend .glow-right;
+
+  .sidebar-heading {
+    padding: 0.875rem 1.25rem;
+    font-size: 1.15rem;
+  }
+
+  .list-group {
+    width: 15rem;
+  }
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+#page-content-wrapper {
+  min-width: 100vw;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+#wrapper.toggled #sidebar-wrapper {
+  margin-left: 0;
+}
+
+.list-group-item {
+  color: $white;
+  background-color: $black !important;
+  border-bottom: none !important;
+}
+
+.list-group-item:hover {
+  color: $white !important;
+  background-color: $dark-gray !important;
+  text-decoration: none !important;
+}
+
+.list-group-item.active {
+  @extend .glow-center;
+  background-color: $dark-gray !important;
+}
+
+@media (min-width: 768px) {
+  #sidebar-wrapper {
+    margin-left: 0;
+  }
+
+  #page-content-wrapper {
+    min-width: 0;
+    width: 100%;
+  }
+
+  #wrapper.toggled #sidebar-wrapper {
+    margin-left: -15rem;
+  }
 }
 </style>
