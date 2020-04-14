8811ac7ec25517da43123c1bded5586536412772
Florian Birk <birk.florian95@gmail.com>
2020-04-14 15:20:14 +0200

    refactored views

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
@@ -8,16 +7,22 @@ const routes = [
   {
     path: "/",
     name: "Home",
-    component: Home
+    redirect:"/video"
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/video",
+    name: "Video",
+    component: () =>import( "../views/Video.vue")
+  },
+  {
+    path: "/music",
+    name: "Music",
+    component: () =>import( "../views/Music.vue")
+  },
+  {
+    path: "/settings",
+    name: "Settings",
+    component: () =>import("../views/Settings.vue")
   }
 ];
 
