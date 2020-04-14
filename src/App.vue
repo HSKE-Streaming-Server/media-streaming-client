6722c4b0cc1c1bf30187922d2ec91575af29501e
Florian Birk <birk.florian95@gmail.com>
2020-04-14 16:31:48 +0200

    little polishing

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,11 @@
 
       <div id="page-content-wrapper">
         <div class="container-fluid">
-          <button
-            class="btn btn-primary float-right"
-            id="menu-toggle"
-            @click="toggleMenu"
-          >Toggle Menu</button>
+          <button id="menu-toggle" @click="toggleMenu">
+            <span id="span1"></span>
+            <span id="span2"></span>
+            <span id="span3"></span>
+          </button>
           <router-view />
         </div>
       </div>
@@ -23,7 +23,6 @@
 import Sidebar from "@/components/Sidebar.vue";
 
 export default {
-
   name: "App",
   components: {
     Sidebar
@@ -37,7 +36,21 @@ export default {
   methods: {
     toggleMenu() {
       this.isToggled = !this.isToggled;
+    },
+    myEventHandler(e) {
+      if (e.target.innerWidth > 768) {
+        this.isToggled = false;
+      }
     }
+  },
+  beforeCreate(){
+    this.isToggled = window.innerWidth <= 768;
+  },
+  created() {
+    window.addEventListener("resize", this.myEventHandler);
+  },
+  destroyed() {
+    window.removeEventListener("resize", this.myEventHandler);
   }
 };
 </script>
@@ -71,13 +84,63 @@ body {
   border-right: none !important;
 }
 
+#menu-toggle {
+  padding:0.1rem;
+  background: none ;
+  border:none;
+  span{
+    width:2rem;
+    margin:0.2rem;
+    border: 0.21rem solid;
+    display:block;
+  }
+  #span1{
+    border-color: $neon-blue;
+  }
+  #span2{
+    border-color: $neon-blue-green;
+  }
+  #span3{
+    border-color: $neon-green;
+  }
+}
+
+#menu-toggle:focus{
+  border:none;
+  outline: none;
+}
+
+#menu-toggle:hover{
+  #span1{
+    -webkit-box-shadow:  2px 2px 3px 0px $neon-blue;
+    -moz-box-shadow:  2px 2px 3px 0px $neon-blue;
+    box-shadow:  2px 2px 3px 0px $neon-blue;
+  }
+  #span2{
+    -webkit-box-shadow:  2px 2px 3px 0px $neon-blue-green;
+    -moz-box-shadow:  2px 2px 3px 0px $neon-blue-green;
+    box-shadow:  2px 2px 3px 0px $neon-blue-green;
+  }
+  #span3{
+    -webkit-box-shadow: 2px 2px 3px 0px $neon-green;
+    -moz-box-shadow: 2px 2px 3px 0px $neon-green;
+    box-shadow: 2px 2px 3px 0px $neon-green;
+  }
+}
+
 #page-content-wrapper {
   min-width: 100vw;
+  height:100vh;
+  overflow-y: auto;
 }
 
 #wrapper.toggled #sidebar-wrapper {
   margin-left: 0;
 }
+
+// #wrapper.toggled #menu-toggle {
+//     display: none;
+// }
 
 @media (min-width: 768px) {
   #sidebar-wrapper {
@@ -91,6 +154,10 @@ body {
 
   #wrapper.toggled #sidebar-wrapper {
     margin-left: -15rem;
+  }
+
+  #menu-toggle {
+    display: none;
   }
 }
 </style>
