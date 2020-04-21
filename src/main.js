a43cc40c7ab8bacbd4870539d9e0cefa398d1e23
hani <hani.fares93@gmail.com>
2020-04-21 18:56:12 +0200

    add video.js and dashjs packet, add StreamsServices.js and Modules

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "video.js/dist/video-js.css";
+import "dashjs/dist/dash.all.debug";
+import videojs from "video.js";
 
+Vue.use(videojs); // If not work, then import it in the MediaPlayer Component // maybe the "import "videojs-contrib-dash/dist/videojs-dash";" will be missed
 Vue.config.productionTip = false;
 
 new Vue({
