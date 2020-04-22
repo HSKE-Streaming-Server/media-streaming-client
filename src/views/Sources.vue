4dfb57759720e5b29730c9355b535b598dcd981e
Florian Birk <birk.florian95@gmail.com>
2020-04-22 20:27:13 +0200

    implemented tile

--- src/views/Sources.vue
+++ src/views/Sources.vue
@@ -4,13 +4,24 @@
       <h1>{{type}} Sources</h1>
       <div class="row">
         <div class="col-md-4">
-          <Tile></Tile>
+          <Tile 
+          text="test1" 
+          url="/contents/own"
+          ></Tile>
         </div>
         <div class="col-md-4">
-          <Tile></Tile>
+          <Tile
+            text="test2"
+            url="/contents/ZDF"
+            imageSrc="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/ZDF_logo.svg/1280px-ZDF_logo.svg.png"
+          ></Tile>
         </div>
         <div class="col-md-4">
-          <Tile></Tile>
+          <Tile
+            text="test3"
+            url="/contents/DMAX"
+            imageSrc="https://sonic-eu1-prod-images.disco-api.com/2017/12/14/55316bd1-5f1e-43df-a886-80ebbb7b6699.png"
+          ></Tile>
         </div>
       </div>
     </div>
