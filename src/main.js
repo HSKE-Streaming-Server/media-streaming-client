65f1dc42aae15eabb32450a8cced3d4a19f1ed93
hani <hani.fares93@gmail.com>
2020-06-13 14:09:50 +0200

    design NotFound.vue

--- src/main.js
+++ src/main.js
@@ -7,11 +7,15 @@ import "dashjs/dist/dash.all.debug";
 import "dashjs/dist/dash.all.min"
 import "videojs-contrib-dash/dist/videojs-dash.min";
 import "nprogress/nprogress.css"
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 //import videojs from "video.js";
 import VueResource from 'vue-resource'
 Vue.use(VueResource);
 
-
+library.add(faSpinner)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import carousel from "vue-carousel"
